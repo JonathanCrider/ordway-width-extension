c5fce37e201cb6f5e19ebac7eda3313cae56f86c
jonathan-fsa <87999690+jonathan-fsa@users.noreply.github.com>
2022-09-16 08:37:43 -0400

    type check and floor value for user input

--- js/options.js
+++ js/options.js
@@ -1,31 +1,35 @@
 // saves options to chrome storage and sets css width property
-function save_options() {
+const saveOptions = () => {
   
   const _width = document.getElementById('set-width')
-  const widthValue = _width.value + 'px'
+  const toNum = parseInt(_width.value)
+  const isNum = !isNaN(toNum)
+  // value check for int, sets default if not
+  const floorNum = isNum ? Math.floor(toNum) : 1800
+  const widthPixelValue = floorNum + 'px'
   
   chrome.storage.sync.set({
-    ordwayCustomerTableWidth: _width.value
-  }, function() {
+    ordwayCustomerTableWidth: floorNum
+  }, () => {
     // update status
     const status = document.getElementById('status')
-    status.textContent = `Width saved as ${widthValue}`
-    setTimeout(function() {
+    status.textContent = isNum ? `Width saved as ${widthPixelValue}` : 'Not a number, saved as default 1800px'
+    setTimeout(() => {
       status.textContent = ''
     }, 2000)
   })
 }
 
-function restore_options() {
+const restoreOptions = () => {
   chrome.storage.sync.get({
     ordwayCustomerTableWidth: 1800
-  }, function(items) {
+  }, (items) => {
     document.getElementById('set-width').value = items.ordwayCustomerTableWidth
   })
 }
 
-document.addEventListener('DOMContentLoaded', restore_options)
-document.getElementById('save').addEventListener('click', save_options)
-document.getElementById('set-width').addEventListener('keypress', function(e) {
-  if (e.key === 'Enter') save_options()
+document.addEventListener('DOMContentLoaded', restoreOptions)
+document.getElementById('save').addEventListener('click', saveOptions)
+document.getElementById('set-width').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') saveOptions()
 })
